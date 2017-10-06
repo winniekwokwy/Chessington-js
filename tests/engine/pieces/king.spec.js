2b123ff7d30d1351442efa5ad3ee1381d862d7f6
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 14:49:17 +0100

    merged changes

--- tests/engine/pieces/king.spec.js
+++ tests/engine/pieces/king.spec.js
@@ -2,4 +2,40 @@ import King from '../../../src/engine/pieces/king';
 
 describe('King', () => {
 
+    let board;
+    beforeEach(() => board = new Board());
+
+    it('can move to adjacent squares', () => {
+        const king = new King(Player.WHITE);
+        board.setPiece(Square.at(3, 4), king);
+
+        const moves = king.getAvailableMoves(board);
+
+        const expectedMoves = [
+            Square.at(2, 3), Square.at(2, 4), Square.at(2, 5), Square.at(3, 5),
+            Square.at(4, 5), Square.at(4, 4), Square.at(4, 3), Square.at(3, 3)
+        ];
+
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('cannot make any other moves', () => {
+        const king = new King(Player.WHITE);
+        board.setPiece(Square.at(3, 4), king);
+
+        const moves = king.getAvailableMoves(board);
+
+        moves.should.have.length(8);
+    });
+
+    it('cannot leave the board', () => {
+        const king = new King(Player.WHITE);
+        board.setPiece(Square.at(0, 0), king);
+
+        const moves = king.getAvailableMoves(board);
+
+        const expectedMoves = [Square.at(0, 1), Square.at(1, 1), Square.at(1, 0)];
+
+        moves.should.deep.have.members(expectedMoves);
+    });
 });
