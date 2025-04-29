faa00aa904d2ab199e5b59e4112c5d0f88d633c0
Rachel Kelly <rachel.kelly@softwire.com>
2025-04-29 15:03:57 +0100

    further pawn tests passing

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -14,19 +14,41 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        const availableMoves = [];
+
+        let availableMoves = [];
         let location = board.findPiece(this);
+        const firstSquareWhite = Square.at(location.row + 1, location.col)
+        const secondSquareWhite = Square.at(location.row + 2, location.col)
+        const firstSquareBlack = Square.at(location.row - 1, location.col)
+        const secondSquareBlack = Square.at(location.row - 2, location.col)
+
+        console.log(board, "<<< board", location, "<<<<location");
+
         if (this.player === Player.WHITE) {
-            availableMoves.push(Square.at(location.row + 1, location.col));
-            if (this.firstMove) {
-                availableMoves.push(Square.at(location.row + 2, location.col));
+            
+            if (this.checkIfEmpty(board, firstSquareWhite)) {
+                availableMoves.push(firstSquareWhite);
+            }
+            if (this.firstMove && this.checkIfEmpty(board, firstSquareWhite) && this.checkIfEmpty(board, secondSquareWhite)) {
+                availableMoves.push(secondSquareWhite);
             }
         } else {
-            availableMoves.push(Square.at(location.row - 1, location.col));
-            if (this.firstMove) {
-                availableMoves.push(Square.at(location.row - 2, location.col));
+            if (this.checkIfEmpty(board, firstSquareBlack)) {
+                availableMoves.push(firstSquareBlack);
+            
+            }
+            if (this.firstMove && this.checkIfEmpty(board, firstSquareBlack) && this.checkIfEmpty(board, secondSquareBlack)) {
+                availableMoves.push(secondSquareBlack);
             }
         }
         return availableMoves;
     }
+
+    checkIfEmpty(board, square) {
+        return !board.getPiece(square);
+    }
 }
+
+
+//we need to check if there are any pieces already at that location
+
