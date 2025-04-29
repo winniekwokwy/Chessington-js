56b3e16a6c9697f04da9f10b9fc61ce079ece43a
Rachel Kelly <rachel.kelly@softwire.com>
2025-04-29 16:06:38 +0100

    mid solution

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -11,12 +11,40 @@ export default class Rook extends Piece {
         //return new Array(0);
         const rookMoves = [];
         let location = board.findPiece(this);
+        let b
 
-        for (let row = 0 ; row < GameSettings.BOARD_SIZE; row++) {
-            if (row !==location.row) {
-                rookMoves.push(Square.at(row, location.col));
-            }
+        for (let row = location.row + 1 ; row < GameSettings.BOARD_SIZE; row++) {
+            let square = Square.at(row, location.col);
+            let piece = board.getPiece(square);
+            if (!piece) {
+                rookMoves.push(square);
+            } else {
+                if (piece.plyer !== this.player) rookMoves.push(square)
+                    break;
+                }
         }
+        for (let row = location.row - 1 ;row >= 0; row--) {
+            let square = Square.at(row, location.col);
+            let piece = board.getPiece(square);
+            if (!piece) {
+                rookMoves.push(square);
+            } else {
+                if (piece.plyer !== this.player) rookMoves.push(square)
+                    break;
+                }
+        }
+        for (let col = location.col + 1 ; GameSettings.BOARD_SIZE; col++) {
+            let square = Square.at(location.row, col);
+            let piece = board.getPiece(square);
+            if (!piece) {
+                rookMoves.push(square);
+            } else {
+                if (piece.plyer !== this.player) rookMoves.push(square)
+                    break;
+                }
+        }
+
+
         for (let col = 0; col < GameSettings.BOARD_SIZE; col++) {
             if (col !==location.col) {
                 rookMoves.push(Square.at(location.row, col));
@@ -25,3 +53,4 @@ export default class Rook extends Piece {
         return rookMoves;
     }
 }
+
