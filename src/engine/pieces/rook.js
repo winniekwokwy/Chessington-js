10e8ab5d6959e75ddda48a486c78243b2952cff1
Rachel Kelly <rachel.kelly@softwire.com>
2025-04-29 14:00:44 +0100

    rook tests pass

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +8,20 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        //return new Array(0);
+        const rookMoves = [];
+        let location = board.findPiece(this);
+
+        for (let row = 0 ; row < GameSettings.BOARD_SIZE; row++) {
+            if (row !==location.row) {
+                rookMoves.push(Square.at(row, location.col));
+            }
+        }
+        for (let col = 0; col < GameSettings.BOARD_SIZE; col++) {
+            if (col !==location.col) {
+                rookMoves.push(Square.at(location.row, col));
+            }
+        }
+        return rookMoves;
     }
 }
