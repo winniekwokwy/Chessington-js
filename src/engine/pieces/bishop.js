a9eda54e32cd3e4a629440b1a0ad11e18d50bf05
Rachel Kelly <rachel.kelly@softwire.com>
2025-04-30 12:03:00 +0100

    bishop tests pass

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +8,52 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        // return new Array(0);
+        let bishopMoves = []
+        let location = board.findPiece(this);
+        let square;
+        let piece;
+        let row;
+        let col;
+
+
+        //diagonally right-down
+        row = location.row + 1, col = location.col -1
+        while (row >= 0 && col >=0) {
+            bishopMoves.push(Square.at(row,col))
+            row++,
+            col--;
+        }
+    
+    
+        //diagonally right-up
+        row = location.row + 1, col = location.col +1
+        while (row >= 0 && col < GameSettings.BOARD_SIZE) {
+            bishopMoves.push(Square.at(row,col))
+            row++,
+            col++;
+        }
+
+        //diagonally left-up
+        row = location.row - 1, col = location.col +1
+        while (row >= 0 && col < GameSettings.BOARD_SIZE) {
+            bishopMoves.push(Square.at(row,col))
+            row--,
+            col++;
+        }
+
+        //diagonally left-down
+        row = location.row - 1, col = location.col -1
+        while (row >= 0 && col >=0) {
+            bishopMoves.push(Square.at(row,col))
+            row--,
+            col--;
+        }
+        return bishopMoves;
+    
     }
 }
+
+
+
+
