deed7fbc50db2aea90fab129c8a566d35fc3ac61
Winnie Kwok <winnie.kwok@softwire.com>
2025-04-30 11:59:12 +0100

    Bishop moves diagionally (Red-4)

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +8,40 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const moves = [];
+        let location = board.findPiece(this);
+        let row=location.row;
+        let col=location.col;
+
+        while (row<GameSettings.BOARD_SIZE-1 && col<GameSettings.BOARD_SIZE-1){
+            row++;
+            col++;
+            moves.push(Square.at(row, col));
+        }
+
+        row = location.row;
+        col = location.col;
+        while (row<GameSettings.BOARD_SIZE-1 && col>0){
+            row++;
+            col--;
+            moves.push(Square.at(row, col));
+        }
+
+        row = location.row;
+        col = location.col;
+        while (row>0 && col<GameSettings.BOARD_SIZE-1){
+            row--;
+            col++;
+            moves.push(Square.at(row, col));
+        }
+
+        row = location.row;
+        col = location.col;
+        while (row>0 && col>0){
+            row--;
+            col--;
+            moves.push(Square.at(row, col));
+        }
+        return moves;
     }
 }
